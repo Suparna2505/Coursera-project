b41ff641ad6e102378fc795e696a04195895fe4a
Suparna2505 <67553034+Suparna2505@users.noreply.github.com>
2020-07-08 23:20:24 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,6 +1,6 @@
-(function assignment() {
+(function a() {
 
-	var Wish = ["Suparna", "Jerry", "Suvam", "Jason", "Subhojit", "Arka", "Harry", "Priya", "Himadri", "Jasmin"];
+	var Wish = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var j = 0; j < Wish.length; j++) {
 		if (Wish[j].charAt(0).toLowerCase() === 'j') {
 			byeSpeaker.speak(Wish[j]);
